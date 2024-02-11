1bbb608ca7121d0d295715e4ead4752b7a783fbf
Filiberto Zurita <fmasz6@gmail.com>
2024-02-10 20:16:08 -0400

    List of dogs pet stores

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React, {useState} from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from "react";
 
-function App(props) {
-  const currDate = new Date();
-  const [counter, setCounter] = useState(0);
+const App = (props)=> {
+    //define the state and the setter
+    const [APIlist, setAPIlist] = useState();
 
-  let incrementCounter = ()=> {
-    setCounter(counter+1);
-  }
-  let colorStyle = {color:props['color'],fontSize:props['size']}
+    //On load invoke method to genrate list
+    useEffect(() => {  let url = "https://api.publicapis.org/entries?category=Animals";
+    axios({
+      method: 'get',
+      url: url,
+      responseType: 'json'
+    }).then(resp => {
+        let listOfEntries = resp.data.entries;
+        let listOfEntriesAsArray = Object.entries(listOfEntries);
+        let i=1;
+        let entryDetails = listOfEntriesAsArray.map((entryDetail)=>{
+          return(
+          <li key={i++}>
+            <a href={entryDetail[1]["Link"]} target="_blank" rel="noreferrer">{entryDetail[1]["API"]}</a>
+            </li>
+          )
+        })
+        setAPIlist(entryDetails);
+      })
+    .catch(err => {
+        console.log(err.toString())
+    });
+    }, []);
 
 
+    const colorStyle = { color:props['color'], fontSize:props['size']}
 
   return (
+      <div>
+        <h2>APIs List</h2>
+        <br/>
 
-  <div>
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
-      <h2>The date today is {currDate.toLocaleDateString() }.</h2>
+            <div style={colorStyle}><ul>{APIlist}</ul></div>
     </div>
-
-    <div style={colorStyle}>
-        React Component
-        <br /><br />
-        <button onClick={incrementCounter}>Click Me!</button>
-        <br /><br />
-        {counter}
-      </div>
-  </div>
-
-
-  );
+);
 }
 
 export default App;
-
